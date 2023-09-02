0aafa138184c151b69aa213ec581e4b4ac83a816
ie23s <admin@ie23s.com>
2023-09-02 22:44:07 +0300

    US-5

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,19 +1,27 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.unisup"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+//    implementation("org.springframework.boot:spring-boot-starter")
+//    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
